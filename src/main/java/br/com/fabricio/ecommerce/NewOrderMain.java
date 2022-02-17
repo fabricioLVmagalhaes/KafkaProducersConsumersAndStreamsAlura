089316e8b9ad0785ced0083f7251b12f3f7ac1a7
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2022-02-16 21:36:10 -0300

    extrai camada de producer

--- src/main/java/br/com/fabricio/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/fabricio/ecommerce/NewOrderMain.java
@@ -1,47 +1,19 @@
 package br.com.fabricio.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        for(var i = 0; i < 10; i++) {
-            var producer = new KafkaProducer<String, String>(properties());
-            var key = UUID.randomUUID().toString();
-            var value = key + "1234,67523,78098098";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
-                }
-                System.out.println("sucesso enviando " + data.topic() + ":::particao " + data.partition() + " /offset " + data.offset() + " /timestamp " + data.timestamp());
-            };
-            var email = "Thank You! We are processing your order!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-            producer.send(record, callback).get();
-            producer.send(emailRecord, callback).get();
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + "1234,67523,78098098";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var email = "Thank You! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
         }
-
-
     }
-
-    private static Properties properties() {
-        var properties = new Properties();
-
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
-    }
-
 }

c29bc3cc0f69e9d080db7de6650212c7078beff0
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2022-02-17 21:19:34 -0300

    deserializacao customizada

--- src/main/java/br/com/fabricio/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/fabricio/ecommerce/FraudDetectorService.java
@@ -2,17 +2,21 @@ package br.com.fabricio.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorService {
 
     public static void main(String[] args) {
 
         var fraudService = new FraudDetectorService();
-        try (var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER", fraudService::parse, Order.class, Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("-------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
--- src/main/java/br/com/fabricio/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/fabricio/ecommerce/NewOrderMain.java
@@ -8,14 +8,14 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var oerderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
                 for (var i = 0; i < 10; i++) {
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
                     var order = new Order(userId, orderId, amount);
                     oerderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
-                    var email = "Thank You! We are processing your order!";
+                    var email = new Email("ECOMMERCE_NEW_ORDER", "Thank You! We are processing your order!");
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
                 }
             }

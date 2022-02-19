2dfbac5d9772ae7f6cb418ded381f0dcb9c0d3f4
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2022-02-18 21:39:52 -0300

    novos consumidores e produtores

---
+++ service-user/src/main/java/br/com/fabricio/ecommerce/Order.java
@@ -0,0 +1,19 @@
+package br.com.fabricio.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+    private final String userId, orderID;
+    private final BigDecimal amount;
+
+    public Order(String userId, String orderID, BigDecimal value) {
+        this.userId = userId;
+        this.orderID = orderID;
+        this.amount = value;
+    }
+
+
+    public String getEmail() {
+        return "email";
+    }
+}
